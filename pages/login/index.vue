9b5ca58d60efdcccff0a3637ab0debdccc5c8578
yoha0417 <yoshitohama@gmail.com>
2022-08-21 23:20:19 +0900

    ログイン機能を実装

---
+++ pages/login/index.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <Head>
+            <Title>{{ title }}</Title>
+        </Head>
+        <NuxtLayout>
+            <!--未認証時：ログインボタン-->
+            <div class="container">
+                <div class="row animal-items-center justify-content-center">
+                    <div class="mt-5 p-2 bd-highlight">
+                            <button class="btn btn-info mx-2" type="button" @click="login">Twitterアカウントでログイン</button>
+                    </div>
+                </div>
+            </div>
+        </NuxtLayout>
+    </div>
+</template>
+
+<script setup>
+const title = ref('ログイン')
+
+const login = () => {
+        window.location.href = "http://localhost:8080/twiiter/login"
+    }
+
+
+</script>
---
+++ pages/user/[id].vue
@@ -0,0 +1,11 @@
+<template>
+<div>{{id}}</div>
+</template>
+<script setup>
+import axios from 'axios'
+
+const route = useRoute();
+
+const id = ref(route.params.id)
+
+</script>
